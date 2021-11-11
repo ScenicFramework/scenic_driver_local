ecb1aecebdd800d2bf314fe56b06ea29bfbb9b4a
Vacarsu <kasvaca@gmail.com>
2021-11-11 02:09:54 -0800

    send viewport reshape input when changed

--- lib/from_port.ex
+++ lib/from_port.ex
@@ -79,6 +79,8 @@ defmodule Scenic.Driver.Local.FromPort do
       |> Scenic.Driver.Local.set_global_tx()
       |> Driver.request_update()
 
+    send_input(driver, {:viewport, {:reshappe, {win_width, win_height}}})
+
     {:noreply, driver}
   end
 
