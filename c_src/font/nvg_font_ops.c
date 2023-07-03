54d897213d15b4d011acc59a02c311a6cec42a90
Jon Ringle <jringle@gridpoint.com>
2023-07-03 17:58:36 -0400

    NVG ops

--- c_src/font/nvg_font_ops.c
+++ c_src/font/nvg_font_ops.c
@@ -1,6 +1,3 @@
-#include <string.h>
-
-#include "comms.h"
 #include "font_ops.h"
 #include "nanovg.h"
 
@@ -10,24 +7,4 @@ int32_t font_ops_create(void* v_ctx, font_t* p_font, uint32_t size)
   return nvgCreateFontMem(p_ctx,
                           p_font->id.p_data, p_font->blob.p_data, size,
                           false); // tells nvg to NOT free p_font->blob.p_data when releasing font
-}
-
-//---------------------------------------------------------
-void set_font(sid_t id, void* v_ctx)
-{
-  NVGcontext* p_ctx = (NVGcontext*)v_ctx;
-  // unfortunately, nvgFindFont expects a zero terminated C string
-  char* p_font = calloc(1, id.size + 1);
-  if ( !p_font ) {
-    log_error("Unable to alloc temp font id buffer");
-    return;
-  }
-  memcpy(p_font, id.p_data, id.size);
-
-  int font_id = nvgFindFont(p_ctx, p_font);
-  if (font_id >= 0) {
-    nvgFontFaceId(p_ctx, font_id);
-  }
-
-  free( p_font );
 }
--- c_src/image/nvg_image_ops.c
+++ c_src/image/nvg_image_ops.c
@@ -1,5 +1,4 @@
 #include "comms.h"
-#include "image.h"
 #include "image_ops.h"
 #include "nanovg.h"
 
@@ -21,80 +20,4 @@ void image_ops_delete(void* v_ctx, uint32_t image_id)
 {
   NVGcontext* p_ctx = (NVGcontext*)v_ctx;
   nvgDeleteImage(p_ctx, image_id);
-}
-
-//=============================================================================
-// called when rendering scripts
-
-//---------------------------------------------------------
-void set_fill_image(void* v_ctx, sid_t id)
-{
-  NVGcontext* p_ctx = (NVGcontext*)v_ctx;
-  // get the mapped nvg_id for this image_id
-  image_t* p_image = get_image(id);
-  if (!p_image) return;
-
-  // get the dimensions of the image
-  int w,h;
-  nvgImageSize(p_ctx, p_image->image_id, &w, &h);
-
-
-  // the image is loaded and ready for use
-  nvgFillPaint(p_ctx,
-               nvgImagePattern(p_ctx, 0, 0, w, h, 0, p_image->image_id, 1.0));
-}
-
-//---------------------------------------------------------
-void set_stroke_image(void* v_ctx, sid_t id)
-{
-  NVGcontext* p_ctx = (NVGcontext*)v_ctx;
-  // get the mapped nvg_id for this image_id
-  image_t* p_image = get_image(id);
-  if (!p_image) return;
-
-  // get the dimensions of the image
-  int w,h;
-  nvgImageSize(p_ctx, p_image->image_id, &w, &h);
-
-  // the image is loaded and ready for use
-  nvgStrokePaint(p_ctx,
-                 nvgImagePattern(p_ctx, 0, 0, w, h, 0, p_image->image_id, 1.0));
-}
-
-//---------------------------------------------------------
-// see: https://github.com/memononen/nanovg/issues/348
-void draw_image(void* v_ctx, sid_t id,
-                float sx, float sy, float sw, float sh,
-                float dx, float dy, float dw, float dh)
-{
-  NVGcontext* p_ctx = (NVGcontext*)v_ctx;
-  float ax, ay;
-  NVGpaint img_pattern;
-
-  // get the mapped nvg_id for this driver_id
-  image_t* p_image = get_image(id);
-  if (!p_image) return;
-
-  // get the dimensions of the image
-  int iw,ih;
-  nvgImageSize(p_ctx, p_image->image_id, &iw, &ih);
-
-  // Aspect ration of pixel in x an y dimensions. This allows us to scale
-  // the sprite to fill the whole rectangle.
-  ax = dw / sw;
-  ay = dh / sh;
-
-  // create the temporary pattern
-  img_pattern = nvgImagePattern(p_ctx,
-                                dx - sx*ax, dy - sy*ay, (float)iw*ax, (float)ih*ay,
-                                0, p_image->image_id, 1.0);
-
-  // draw the image into a rect
-  nvgBeginPath(p_ctx);
-  nvgRect(p_ctx, dx, dy, dw, dh);
-  nvgFillPaint(p_ctx, img_pattern);
-  nvgFill(p_ctx);
-
-  // the data for the paint pattern is a struct on the stack.
-  // no need to clean it up
 }
