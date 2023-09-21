a5850640f9b7c36c36e77a3d5d58b3bae075ed32
Jon Ringle <jringle@gridpoint.com>
2023-09-20 22:30:33 -0400

    cairo: Add support for 8, 15, 16, 24 and 32 bits per pixel video formats

--- c_src/device/cairo.c
+++ c_src/device/cairo.c
@@ -28,6 +28,52 @@ cairo_fb_t g_cairo_fb = {0};
 extern device_info_t g_device_info;
 extern device_opts_t g_opts;
 
+// Support for 8 bits per pixel colors
+unsigned short red[256], green[256], blue[256];
+struct fb_cmap map332 = {0, 256, red, green, blue, NULL};
+unsigned short red_b[256], green_b[256], blue_b[256];
+struct fb_cmap map_back = {0, 256, red_b, green_b, blue_b, NULL};
+
+void make332map(struct fb_cmap *map)
+{
+  int rs, gs, bs, i;
+  int r = 8, g = 8, b = 4;
+
+  map->red = red;
+  map->green = green;
+  map->blue = blue;
+
+  rs = 256 / (r - 1);
+  gs = 256 / (g - 1);
+  bs = 256 / (b - 1);
+
+  for (i = 0; i < 256; i++) {
+    map->red[i]   = (rs * ((i / (g * b)) % r)) * 255;
+    map->green[i] = (gs * ((i / b) % g)) * 255;
+    map->blue[i]  = (bs * ((i) % b)) * 255;
+  }
+}
+
+void set8map(int fh, struct fb_cmap *map)
+{
+    if (ioctl(fh, FBIOPUTCMAP, map) < 0) {
+        log_error("cairo: Error putting colormap");
+    }
+}
+
+void get8map(int fh, struct fb_cmap *map)
+{
+    if (ioctl(fh, FBIOGETCMAP, map) < 0) {
+        log_error("cairo: Error getting colormap");
+    }
+}
+
+void set332map(int fh)
+{
+    make332map(&map332);
+    set8map(fh, &map332);
+}
+
 int device_init(const device_opts_t* p_opts,
                 device_info_t* p_info,
                 driver_data_t* p_data)
@@ -62,6 +108,32 @@ int device_init(const device_opts_t* p_opts,
     return -1;
   }
 
+  size_t pix_count = g_cairo_fb.var.xres * g_cairo_fb.var.yres;
+  switch (g_cairo_fb.var.bits_per_pixel)
+  {
+  case 8:
+    p_ctx->fbbuff.c = (uint8_t*)malloc(pix_count * sizeof(uint8_t));
+    break;
+  case 15:
+  case 16:
+    p_ctx->fbbuff.c = (uint8_t*)malloc(pix_count * sizeof(uint16_t));
+    break;
+  case 24:
+    p_ctx->fbbuff.c = (uint8_t*)malloc(pix_count * 3 * sizeof(uint8_t));
+    break;
+  case 32:
+    p_ctx->fbbuff.c = (uint8_t*)malloc(pix_count * sizeof(uint32_t));
+    break;
+  default:
+    log_error("cairo: Unsupported video mode: %dbpp", g_cairo_fb.var.bits_per_pixel);
+    return -1;
+  }
+
+  if (g_cairo_fb.var.bits_per_pixel == 8) {
+    get8map(g_cairo_fb.fd, &map_back);
+    set332map(g_cairo_fb.fd);
+  }
+
   p_info->width = g_cairo_fb.var.xres;
   p_info->height = g_cairo_fb.var.yres;
 
@@ -89,9 +161,15 @@ int device_close(device_info_t* p_info)
   if (g_opts.debug_mode) {
     log_info("cairo %s", __func__);
   }
+
+  if (g_cairo_fb.var.bits_per_pixel == 8) {
+    set8map(g_cairo_fb.fd, &map_back);
+  }
+
   close(g_cairo_fb.fd);
 
   scenic_cairo_ctx_t* p_ctx = (scenic_cairo_ctx_t*)p_info->v_ctx;
+  free(p_ctx->fbbuff.c);
   cairo_surface_destroy(p_ctx->surface);
   free(p_ctx);
 }
@@ -125,23 +203,126 @@ void device_begin_cursor_render(driver_data_t* p_data)
   cairo_translate(p_ctx->cr, p_data->cursor_pos[0], p_data->cursor_pos[1]);
 }
 
-void render_cairo_surface_to_fb(int fd, cairo_surface_t* surface)
+inline static uint8_t to_8_color(uint8_t r, uint8_t g, uint8_t b)
 {
-  cairo_surface_flush(surface);
-  uint8_t* surface_data = cairo_image_surface_get_data(surface);
+    return ((((r >> 5) & 7) << 5) |
+            (((g >> 5) & 7) << 2) |
+            ((b >> 6) & 3));
+}
 
-  int fb_x = g_cairo_fb.var.xres;
-  int fb_y = g_cairo_fb.var.yres;
-  size_t fb_size = fb_x * fb_y * 3;
-  uint8_t* fbbuff = mmap(NULL, fb_size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+inline static uint16_t to_15_color(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((((r >> 3) & 31) << 10) |
+            (((g >> 3) & 31) << 5)  |
+            ((b >> 3) & 31));
+}
 
-  for(uint32_t i = 0, j = 0; i < fb_size; i += 3, j += 4) {
-    fbbuff[i+0] = surface_data[j+0];
-    fbbuff[i+1] = surface_data[j+1];
-    fbbuff[i+2] = surface_data[j+2];
+inline static uint16_t to_15_color_bgr(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((((b >> 3) & 31) << 10) |
+            (((g >> 3) & 31) << 5)  |
+            ((r >> 3) & 31));
+}
+
+inline static uint16_t to_16_color(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((((r >> 3) & 31) << 11) |
+            (((g >> 2) & 63) << 5)  |
+            ((b >> 3) & 31));
+}
+
+void render_cairo_surface_to_fb(scenic_cairo_ctx_t* p_ctx)
+{
+  cairo_surface_flush(p_ctx->surface);
+  uint8_t* cairo_buff = cairo_image_surface_get_data(p_ctx->surface);
+
+  bool is_bgr555 = ((g_cairo_fb.var.red.offset == 0 &&
+                     g_cairo_fb.var.green.offset == 5 &&
+                     g_cairo_fb.var.blue.offset == 10))
+                    ? true
+                    : false;
+
+  size_t pix_count = g_cairo_fb.var.xres * g_cairo_fb.var.yres;
+
+  int cpp = 0;
+  switch (g_cairo_fb.var.bits_per_pixel)
+  {
+  case 8:
+    cpp = 1;
+    for (uint32_t i = 0, j = 0; i < pix_count; i++, j += 4) {
+      p_ctx->fbbuff.c[i] = to_8_color(cairo_buff[j+2],
+                                      cairo_buff[j+1],
+                                      cairo_buff[j+0]);
+    }
+    break;
+  case 15:
+    cpp = 2;
+    if (is_bgr555) {
+      for (uint32_t i = 0, j = 0; i < pix_count; i++, j += 4) {
+        p_ctx->fbbuff.s[i] = to_15_color_bgr(cairo_buff[j+2],
+                                             cairo_buff[j+1],
+                                             cairo_buff[j+0]);
+      }
+    } else {
+      for (uint32_t i = 0, j = 0; i < pix_count; i++, j += 4) {
+        p_ctx->fbbuff.s[i] = to_15_color(cairo_buff[j+2],
+                                         cairo_buff[j+1],
+                                         cairo_buff[j+0]);
+      }
+    }
+    break;
+  case 16:
+    cpp = 2;
+    if (is_bgr555) {
+      for (uint32_t i = 0, j = 0; i < pix_count; i++, j += 4) {
+        p_ctx->fbbuff.s[i] = to_15_color_bgr(cairo_buff[j+2],
+                                             cairo_buff[j+1],
+                                             cairo_buff[j+0]);
+      }
+    } else {
+      for (uint32_t i = 0, j = 0; i < pix_count; i++, j += 4) {
+        p_ctx->fbbuff.s[i] = to_16_color(cairo_buff[j+2],
+                                         cairo_buff[j+1],
+                                         cairo_buff[j+0]);
+      }
+    }
+    break;
+  case 24:
+    cpp = 3;
+    for (uint32_t i = 0, j = 0; i < (cpp * pix_count); i += 3, j += 4) {
+      p_ctx->fbbuff.c[i+0] = cairo_buff[j+0];
+      p_ctx->fbbuff.c[i+1] = cairo_buff[j+1];
+      p_ctx->fbbuff.c[i+2] = cairo_buff[j+2];
+    }
+    break;
+  case 32:
+    cpp = 4;
+    for (uint32_t i = 0, j = 0; i < pix_count; i++, j += 4) {
+      p_ctx->fbbuff.i[i] = ((cairo_buff[j+2] << 16)) |
+                            (cairo_buff[j+1] << 8) |
+                            (cairo_buff[j+0]);
+    }
+    break;
   }
 
-  munmap(fbbuff, fb_size);
+  uint32_t yc = g_cairo_fb.var.yres_virtual;
+  uint32_t xc = g_cairo_fb.var.xres_virtual;
+  size_t x_stride = (g_cairo_fb.fix.line_length * 8) / g_cairo_fb.var.bits_per_pixel;
+  size_t fb_size = x_stride * yc * cpp;
+  uint8_t* fb = mmap(NULL, fb_size, PROT_WRITE | PROT_READ, MAP_SHARED, g_cairo_fb.fd, 0);
+
+  if (fb == MAP_FAILED) {
+    log_error("cairo: failed to mmap fb");
+    return;
+  }
+
+  uint8_t* p_fb = fb;
+  uint8_t* p_image = p_ctx->fbbuff.c;
+
+  for (uint32_t i = 0; i < yc; i++, p_fb += x_stride * cpp, p_image += xc * cpp)
+    memcpy(p_fb, p_image, xc * cpp);
+
+  munmap(fb, fb_size);
 }
 
 void device_end_render(driver_data_t* p_data)
@@ -151,7 +332,7 @@ void device_end_render(driver_data_t* p_data)
   }
 
   scenic_cairo_ctx_t* p_ctx = (scenic_cairo_ctx_t*)p_data->v_ctx;
-  render_cairo_surface_to_fb(g_cairo_fb.fd, p_ctx->surface);
+  render_cairo_surface_to_fb(p_ctx);
 
   cairo_destroy(p_ctx->cr);
 }
